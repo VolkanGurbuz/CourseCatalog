c0ed359f573cc43ffe746778643bf98a2f8284cd
volkangurbuz <volkangurbuz16@gmail.com>
2020-04-05 19:10:20 +0200

    Initial commit

---
+++ src/test/java/com/volkangurbuz/services/CourseCatalog/CourseCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.volkangurbuz.services.CourseCatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
